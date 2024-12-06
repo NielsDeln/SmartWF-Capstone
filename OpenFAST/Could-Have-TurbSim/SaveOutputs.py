31621c1af77678d4b4cd3a640df4dd5296f54a6a
PakoPoka <maxfoiss@yahoo.com>
2024-12-06 15:10:34 +0100

    Could-Have-model OpenFAST loop with TurbSim

---
+++ OpenFAST/Could-Have-TurbSim/SaveOutputs.py
@@ -0,0 +1,58 @@
+import numpy as np
+import subprocess
+import shutil
+
+# Configuration
+openfast_executable = "openfast_x64.exe"
+turbsim_executable = 'TurbSim_x64.exe'
+output_directory = "../4_Results"  # Directory to store results
+num_simulations = 5  # Number of simulations to run
+min_speed = 10
+max_speed = 25
+speeds = np.arange(min_speed, max_speed, num_simulations)
+
+# Loop to run simulations
+for i in speeds:
+    #Change simulation input
+    shutil.copyfile("1_Configuration/Inflow_files/TurbSim_input.inp", "1_Configuration/Inflow_files/TurbSim_input_temp.inp")
+    with open("1_Configuration/Inflow_files/TurbSim_input_temp.inp", 'r') as wind_file:
+        lines = wind_file.readlines()
+
+    delimiter = '    '
+    parts = lines[39].strip().split(delimiter, maxsplit=2)
+
+    value, param, description = parts
+    # Replace the value for the matching parameter
+    lines[39] = f"{i}{delimiter}{param}{delimiter}{description}\n"
+
+    # Write the updated lines back to the file
+    with open("1_Configuration/Inflow_files/TurbSim_input_temp.inp", "w") as file:
+        file.writelines(lines)
+    shutil.copyfile("1_Configuration/Inflow_files/TurbSim_input_temp.inp", f"4_Results/Input/WS_Sim_{i}.inp")
+
+    turbsim_input = "1_Configuration/Inflow_files/TurbSim_input_temp.inp"
+    # Run Turbsim
+    try:
+        subprocess.run(
+            [turbsim_executable, turbsim_input], 
+            check=True
+        )
+        print(f"TurbSim field {i} generated successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error running simulation {i}: {e}")
+    shutil.copyfile("1_Configuration/Inflow_files/TurbSim_input_temp.bts", f"4_Results/Input/WS_Sim_{i}.bts")
+
+    # Define input/output file paths
+    simulation_input = "1_Configuration\IEA-22MW-RWT\IEA-22-280-RWT-Monopile\IEA-22-280-RWT-Monopile.fst"
+
+    # Run OpenFAST
+    try:
+        subprocess.run(
+            [openfast_executable, simulation_input],
+            check=True
+        )
+        print(f"Simulation {i} completed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error running simulation {i}: {e}")
+    
+    shutil.copy("1_Configuration\IEA-22MW-RWT\IEA-22-280-RWT-Monopile\IEA-22-280-RWT-Monopile.out", f"4_Results/Output/Sim_WS_{i}.out")

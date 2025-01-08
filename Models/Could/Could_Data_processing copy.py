5338c31c555e7884ad11a00294000e1598bd7c62
PakoPoka <maxfoiss@yahoo.com>
2025-01-08 16:39:55 +0100

    data_processing files

--- Models/Could/Could_Data_processing copy.py
+++ Models/Could/Could_Data_processing copy.py
@@ -15,8 +15,12 @@ for file in os.listdir(input_directory)[:400]:  # List all files in the director
         file_path = os.path.join(input_directory, file)  # Create the full path
         TS = openfast_toolbox.io.TurbSimFile(file_path)  # Process the .bts file
         plane = TS['u']
+        plane = plane.astype(np.float32)  # Convert to float32
         data_list.append(plane)
         i += 1
         print(f'file number: {i}')
+        
+# Stack the data and save it in a compressed format
 data = np.stack(data_list, axis=0)
-np.savez_compressed('w5_w9_could_dataset_numpy.npz', data)
+np.savez_compressed('w5_w9_could_dataset_numpy_float32.npz', data)
+

238a35a8c9cd7c2217b0890b08d75315e169d37f
HugoPolder <h.d.h.polder@student.tudelft.nl>
2025-01-10 12:49:40 +0100

    Added 3D scatterplot RF_predictions

--- Models/Must/Traditional AI techniques/RF_must.py
+++ Models/Must/Traditional AI techniques/RF_must.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import mean_absolute_error
 
 must_df = pd.read_csv('..\DEL_must_model_2.csv', sep='\t')
-print(must_df)
+# print(must_df)
 
 y = must_df['Leq_x'].to_numpy()
 X = must_df[['Windspeed', 'STDeV']].to_numpy()
@@ -21,11 +21,11 @@ rf = RandomForestRegressor()
 rf.fit(X_train, y_train)
 predictions_rf = rf.predict(X_test)
 mae_rf = mean_absolute_error(y_test, predictions_rf)
-print(mae_rf)
+print("mean absolute error:", mae_rf)
 
 scores_rf = cross_val_score(rf, X_train, y_train, cv=5, scoring = 'neg_mean_absolute_error')
 mean_rf = np.mean(-scores_rf)
-print(mean_rf)
+print("mean absolute error cross validation",mean_rf)
 
 plt.scatter(y_test, predictions_rf, alpha=0.7, edgecolor='k', label='Predictions')
 plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red', linestyle='--', label='Perfect Fit')
@@ -36,3 +36,27 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
+
+# Scatterplot with all predictions combined
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# First scatter plot
+xs1 = must_df['Windspeed']
+ys1 = must_df['STDeV']
+zs1 = must_df['Leq_x']
+ax.scatter(xs1, ys1, zs1, marker='s', label='Data')
+
+# Second scatter plot
+xs2 = X_test[:,0]
+ys2 = X_test[:,1]
+zs2 = predictions_rf
+ax.scatter(xs2, ys2, zs2, marker='o', label='Random Forest')
+
+# Set labels and title
+ax.set_xlabel('Windspeed')
+ax.set_ylabel('STDev')
+ax.set_zlabel('Leq')
+ax.set_title('3D Scatter Plots Random Forest')
+ax.legend()
+plt.show()
